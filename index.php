5e885f00e7777565e3a47a121b14969ae90b7ad7
Koonri <92974237+Koonri@users.noreply.github.com>
2022-03-22 12:48:22 +0100

    displaying staff names from db

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
 
-echo "Hello world";
+$db = new mysqli("localhost", "root", "", "med");
+
+$q = $db->prepare("SELECT * FROM staff");
+
+
+if($q->execute()) {
+    // wykona jeśli kwerenda jest poprawnie przetworzona przez serwer
+    $result = $q->get_result();
+    while($row = $result->fetch_assoc()) {
+        $firstname = $row['FirstName'];
+        $lastname = $row['Lastname'];
+        echo "Lekarz $firstname $lastname<br>";
+    }
+} else {
+    echo "Błąd podczas wyszukiwania w bazie danych";   
+}
 
 
 ?>
